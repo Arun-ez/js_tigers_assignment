6b33b8285759a89a15d7d31f8d72a43126fb1bda
Arun-ez <arunshaw433@gmail.com>
2023-12-17 01:30:52 +0530

    added all code

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+    env: {
+        MONGODB_URI: 'mongodb+srv://arunshaw433:vendor_db_pass@cluster0.sbfwxmq.mongodb.net/main?retryWrites=true&w=majority',
+        JWT_SECRET_KEY: 'hairhy7883hiedb7847878sjQBSasrf56huioher',
+        GOOGLE_CLIENT_ID: '77757691111-bu5rdll6s21nli2k1ap9v8vqukl0ch4f.apps.googleusercontent.com',
+        GOOGLE_CLIENT_SECRET: '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'
+    }
+}
 
 module.exports = nextConfig
